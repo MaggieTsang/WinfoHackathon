47fba5b12502d924bde1908f26e2014eaf987fbb
Maggie Tsang <mbytsang@gmail.com>
2019-01-12 16:02:00 -0800

    checkboxes on homepage

--- src/CheckboxSelections.js
+++ src/CheckboxSelections.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { FormGroup, Label, Button } from 'reactstrap';
+import { FormGroup, Label, Input, Col } from 'reactstrap';
 
 export default class CheckboxSelections extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+
     let items = this.props.items.map((item) => {
-      return <FormGroup key={item}>item</FormGroup>
+      return <FormGroup key={item}>{item}</FormGroup>
     });
     return (
-      <FormGroup check>
-        <Label check>
-          <Input type="checkbox" />{' '}
-          this.props.topic
-        </Label>
-      </FormGroup>
+      <Col sm="3">
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox" />{' '}
+            {items}
+          </Label>
+        </FormGroup>
+      </Col>
     )
   }
 }
