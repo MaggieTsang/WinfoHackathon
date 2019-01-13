6299e17715dd1ba29446531ee57c2439372e907a
Yulong Tan <yulongtan67@gmail.com>
2019-01-12 18:51:07 -0800

    Updates

---
+++ precache-manifest.8eb96237b07800f1051d549638972583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5064862560e08a1bd78a",
+    "url": "/winfohackathon/static/js/runtime~main.50648625.js"
+  },
+  {
+    "revision": "1cadf5add07068a9e186",
+    "url": "/winfohackathon/static/js/main.1cadf5ad.chunk.js"
+  },
+  {
+    "revision": "34cc4cb8096594d90e6f",
+    "url": "/winfohackathon/static/js/1.34cc4cb8.chunk.js"
+  },
+  {
+    "revision": "1cadf5add07068a9e186",
+    "url": "/winfohackathon/static/css/main.9c777789.chunk.css"
+  },
+  {
+    "revision": "34cc4cb8096594d90e6f",
+    "url": "/winfohackathon/static/css/1.9d959535.chunk.css"
+  },
+  {
+    "revision": "c490dc1367e5beb3bec22b09eff896df",
+    "url": "/winfohackathon/index.html"
+  }
+];
