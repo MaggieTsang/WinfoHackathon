ea2a41918790b4fcc67c2791c1be146934e5ff6c
Yulong Tan <yulongtan67@gmail.com>
2019-01-12 18:47:30 -0800

    Updates

---
+++ precache-manifest.5d269fb9174df39200ae91ab4cb43f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "774b5a5900e96cf55808",
+    "url": "/static/js/main.774b5a59.chunk.js"
+  },
+  {
+    "revision": "34cc4cb8096594d90e6f",
+    "url": "/static/js/1.34cc4cb8.chunk.js"
+  },
+  {
+    "revision": "774b5a5900e96cf55808",
+    "url": "/static/css/main.9c777789.chunk.css"
+  },
+  {
+    "revision": "34cc4cb8096594d90e6f",
+    "url": "/static/css/1.9d959535.chunk.css"
+  },
+  {
+    "revision": "5d865ae136e852eb214a468f295c1ff6",
+    "url": "/index.html"
+  }
+];
